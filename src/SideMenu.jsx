3cd64ed883ab4a02231eaf578b3c8c39d8bd7c08
GabrielH0 <gabrielghb10@hotmail.com>
2021-08-13 00:58:47 -0300

    header

--- src/SideMenu.jsx
+++ src/SideMenu.jsx
@@ -1,13 +1,11 @@
 import React from "react"
-import { useHistory } from "react-router";
 import { Nav } from 'react-bootstrap'
-import './SideMenu.css'
 
 export default function (props) {
     return (
         <Nav bg="dark" style={{ backgroundColor: "#303030", height: "100%" }} className="sideMenu">
             <div style={{ color: "white", width: "100%" }}>
-                <Nav.Link href="/clientes" style={{ marginTop: "15%", color: "white" }}> Cadastro de Cliente</Nav.Link>
+                <Nav.Link href="/clientes" style={{ marginTop: "15%", color: "white" }}> Clientes</Nav.Link>
                 <Nav.Link href="/endereco" style={{ color: "white" }}> Cadastro de Endereço </Nav.Link>
                 <hr />
                 <Nav.Link href="/produto/hardware" style={{ color: "white" }}> Cadastro de Hardware </Nav.Link>
